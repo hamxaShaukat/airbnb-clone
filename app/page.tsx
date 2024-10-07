6a8a7d35c6ed8edbfcae19955dec8f087b048b06
hamxaShaukat <hamzashaukat714@gmail.com>
2024-10-07 23:41:39 +0500

    added a simple navigationbar

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Navbar from "@/components/Navbar/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="">
+    <Navbar />
+   </div>
+  );
+}
