fa9fc7f1607982a24d4f102efa597ae24531d260
hamxashaukat <hamzashaukat714@gmail.com>
2024-11-07 12:29:08 +0500

    Project initialized

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
     darkMode: ["class"],
     content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
@@ -57,5 +63,5 @@ export default {
   	}
   },
   plugins: [require("tailwindcss-animate")],
-}
-
+};
+export default config;
